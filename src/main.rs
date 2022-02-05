c4449b60a860a75bad30d244ab59ab3d06f1ce53
KaranKB1994 <karan.b.gen@gmail.com>
2022-02-05 10:57:49 +0530

    modified from embedded rust book and tested

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-//! Demonstrate the use of a blocking `Delay` using the SYST (sysclock) timer.
-
-#![deny(unsafe_code)]
-#![allow(clippy::empty_loop)]
 #![no_main]
 #![no_std]
 
@@ -9,35 +5,17 @@
 use panic_halt as _; // panic handler
 
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{hprintln};
 use stm32f4xx_hal as hal;
 
-use crate::hal::{pac, prelude::*};
 
 #[entry]
-fn main() -> ! {
-    if let (Some(dp), Some(cp)) = (
-        pac::Peripherals::take(),
-        cortex_m::peripheral::Peripherals::take(),
-    ) {
-        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
-        let gpioa = dp.GPIOA.split();
-        let mut led = gpioa.pa5.into_push_pull_output();
+fn main() -> ! {	
+    hprintln!("Hello, world!").unwrap();
 
-        // Set up the system clock. We want to run at 48MHz for this one.
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    // debug::exit(debug::EXIT_SUCCESS);
 
-        // Create a delay abstraction based on SysTick
-        let mut delay = hal::delay::Delay::new(cp.SYST, &clocks);
-
-        loop {
-            // On for 1s, off for 1s.
-            led.set_high();
-            delay.delay_ms(1000_u32);
-            led.set_low();
-            delay.delay_ms(1000_u32);
-        }
-    }
-
-    loop {}
+    loop {}	
 }
